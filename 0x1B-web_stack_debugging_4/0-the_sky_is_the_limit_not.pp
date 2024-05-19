e27d6bb7e793196b4a8077beda3d73ec66b23dcf
Ana123tase <anastasedukunda@gmail.com>
2024-05-18 22:57:35 -0400

    debug until there is no limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# This manuscript increases the amount of traffic an Nginx server can handle
+# This manuscript increases the amount of traffic beyond an Nginx server can handle
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
